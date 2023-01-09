9769d438375833cd6ff3cc6e5940de0a0096971e
Chirag Singh <75934639+Chiragaug10@users.noreply.github.com>
2023-01-09 21:38:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+
+function App() {
+  return (
+    <ChatEngine
+        height="100vh"
+        projectID="362e94c7-705f-42a8-ad3a-38a8ed61e73b"
+        userName="chirag"
+        userSecret="12345"
+    />
+  );
+}
+
+export default App;
